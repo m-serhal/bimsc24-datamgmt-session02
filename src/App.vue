9a10e2a34a564d764496edb9b3a206fff63a6f13
m-serhal <156105100+m-serhal@users.noreply.github.com>
2024-01-28 23:37:45 +0200

    Vue website
    setting up the base vue website

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,35 @@
 <script setup>
 import { ref } from "vue" // you need this to use ref()
 
+<<<<<<< Updated upstream
 
+=======
+// Define variables and constants
+let count = ref(0);
+let scriptname = ref("addTwoNumbers.gh")
+let name = ref("M.Serhal")
+
+// Define functions
+function increment() {
+  count.value++;
+  console.log(name)
+}
+>>>>>>> Stashed changes
 </script>
 
 
 
 <!-- the template is where the html code goes -->
 <template>
+<<<<<<< Updated upstream
+=======
+  <div id="top-bar">
+    <div id="title-container">
+      <img class="logo-image" alt="Iaac logo" src="./assets/iaac-white.png" />
+      <h2>Digital Tools for Cloud-based Data Management {{ name }} - something</h2>
+    </div>
+  </div>
+>>>>>>> Stashed changes
 
 
 
