b2e99bbaff21c17e29072dcfd4f7f9b419041980
m-serhal <156105100+m-serhal@users.noreply.github.com>
2024-01-28 23:43:44 +0200

    Vue website
    setting up the base vue website

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
-<!-- the script is where the js code goes -->
+<!-- // Script is in some way equivalent to script.js. This is place
+to define variables, methods and imports of other Vue compoennts. -->
 <script setup>
-import { ref } from "vue" // you need this to use ref()
+// Understanding ref article: https://blog.logrocket.com/understanding-vue-refs/#:~:text=Ref%20s%20are%20Vue.,element%20in%20your%20Vue%20instance.
+// When ref attribute is added to element, this element then can be referenced
+// in template. It is sort of templatecement of getElementById (but better)
+import { ref } from "vue";
 
-<<<<<<< Updated upstream
-
-=======
 // Define variables and constants
 let count = ref(0);
 let scriptname = ref("addTwoNumbers.gh")
@@ -15,31 +16,70 @@ function increment() {
   count.value++;
   console.log(name)
 }
->>>>>>> Stashed changes
 </script>
 
-
-
-<!-- the template is where the html code goes -->
+<!-- Template is a HTML-based syntax that allows you to bind the rendered DOM elements
+with data, objects, functions etc. -->
 <template>
-<<<<<<< Updated upstream
-=======
   <div id="top-bar">
     <div id="title-container">
       <img class="logo-image" alt="Iaac logo" src="./assets/iaac-white.png" />
       <h2>Digital Tools for Cloud-based Data Management {{ name }} - something</h2>
     </div>
   </div>
->>>>>>> Stashed changes
 
+  <div id="content">
+    <!-- First example - button -->
+    <!-- Here we define what function will be called when button is clicked. -->
+    <button @click="increment">Add one more</button>
 
+    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+    <p style="margin-left: 8px">Count is: {{ count }}</p>
 
+  </div>
 </template>
 
+<!-- Style is for CSS styling -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
+  color: #2c3e50;
+}
 
-<!-- style is where the css code goes -->
-<style scoped>
+#top-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.7);
+}
 
+#title-container {
+  display: flex;
+  align-items: center;
+  color: white;
+  margin-right: 1.5rem;
+}
 
+#content {
+  display: flex;
+  padding: 20px
+}
+
+.logo-image {
+  height: 3.25rem;
+  padding: 0.5rem;
+}
+
+h2 {
+  font-size: 1.125rem;
+  font-weight: 600;
+  letter-spacing: -0.05em;
+  font-size: 1.125rem;
+  font-weight: 600;
+  letter-spacing: 0.01em;
+}
 </style>
+
